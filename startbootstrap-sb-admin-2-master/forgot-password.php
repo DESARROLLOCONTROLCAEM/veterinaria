5ec66d795f00b4c20f9d28ff3934a8d275e00535
adriana <apatriciarojas@ucundinamarca.edu.co>
2022-05-29 18:45:03 -0500

    recuperar contraseña

--- startbootstrap-sb-admin-2-master/forgot-password.php
+++ startbootstrap-sb-admin-2-master/forgot-password.php
@@ -39,9 +39,9 @@
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-2">Forgot Your Password?</h1>
-                                        <p class="mb-4">We get it, stuff happens. Just enter your email address below
-                                            and we'll send you a link to reset your password!</p>
+                                        <h1 class="h4 text-gray-900 mb-2">Olvidaste tu contraseña?</h1>
+                                        <p class="mb-4">Lo entendemos, pasan cosas. Sólo tienes que introducir tu dirección de correo electrónico a continuación
+                                             y le enviaremos un enlace para restablecer su contraseña!</p>
                                     </div>
                                     <form class="user">
                                         <div class="form-group">
@@ -50,15 +50,15 @@
                                                 placeholder="Enter Email Address...">
                                         </div>
                                         <a href="login.php" class="btn btn-primary btn-user btn-block">
-                                            Reset Password
+                                        Restablecer la contraseña
                                         </a>
                                     </form>
                                     <hr>
                                     <div class="text-center">
-                                        <a class="small" href="register.html">Create an Account!</a>
+                                        <a class="small" href="register.html">Crea una cuenta!</a>
                                     </div>
                                     <div class="text-center">
-                                        <a class="small" href="login.php">Already have an account? Login!</a>
+                                        <a class="small" href="login.php">¿Ya tienes una cuenta? Login!</a>
                                     </div>
                                 </div>
                             </div>
